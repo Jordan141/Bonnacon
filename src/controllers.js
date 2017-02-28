65a8cd3161ca9ab65a83647090815f5a3586ca12
Michael Kurowski <Byamarro@gmail.com>
2017-02-28 05:10:03 +0100

    [controllers] Removed unecessary cb arguments from controllers

--- src/controllers.js
+++ src/controllers.js
@@ -12,23 +12,25 @@ module.exports = {
   loginToSteam: () => {},
   logoutFromSteam: () => {},
 
-  loginToCleverbot: (ctrlObj, cb) => {
+  loginToCleverbot: (ctrlObj) => {
     //loginObj {apiUser, apiKey, username}
     let {username} = ctrlObj
     let bot = new cleverbot(credentials.cleverBotApiUser,credentials.cleverBotApiKey)
     return new Promise( (resolve, reject) => {
       bot.create((err, session) => {
-        if (err) return reject(err)
+        if (err) {
+          console.log('[cleverbot error] An error occured during creating session', err)
+          console.log('[cleverbot error] debug msg: username: ', username)
+          return reject(err)
+        }
         return resolve(bot)
       })
     })
   },
   logoutFromCleverbot: () => {},
-
   sendMessageToSteam: () => {},
   loadMessageFromSteam: () => {},
-
-  sendMessageToCleverbot: (ctrlObj, cb) => {
+  sendMessageToCleverbot: (ctrlObj) => {
     let {session, message} = ctrlObj
     return new Promise ( (resolve, reject) => {
       session.ask(message, (err, response) => {
